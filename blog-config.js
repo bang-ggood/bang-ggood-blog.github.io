12fbc43461933dea732797d8a14802b0f2f7770f
ooherin <555ohr@naver.com>
2024-10-04 15:19:29 +0900

    fix: 수정

--- blog-config.js
+++ blog-config.js
@@ -2,14 +2,14 @@ module.exports = {
   title: "방끗",
   description: "방 구하기 이걸로 끝-! 여러분들의 방 구하기 도우미 방끗입니다.",
   author: "방끗",
-  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
+  siteUrl: "https://bang-ggood.github.io/",
   links: {
     github: "https://github.com/woowacourse-teams/2024-bang-ggood",
     email: "bangggood608@gmail.com",
     link: "https://bang-ggood.com/",
   },
   utterances: {
-    repo: "devHudi/gatsby-starter-hoodie",
+    repo: "bang-ggood.github.io",
     type: "pathname",
   },
 }
